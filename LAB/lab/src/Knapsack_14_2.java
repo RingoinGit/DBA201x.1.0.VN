c99a1a7fdd7db4d4aca67cc852518adb741dcd76
VTM Surface <newsunnc@gmail.com>
2024-12-19 14:59:46 +0900

    Hoàn thành bài lab 14_2

--- LAB/lab/src/Knapsack_14_2.java
+++ LAB/lab/src/Knapsack_14_2.java
@@ -3,15 +3,27 @@ import java.util.*;
 public class Knapsack_14_2 {
     static int optimalWeight(int W, int[] w) {
         //write you code here
-
-
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
+        //Tham khảo trên internet
+        int length = w.length;
+        int[][] result = new int[length + 1][W + 1];
+        for (int i = 0; i <= length; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) {
+                    result[i][j] = 0; //thỏi vàng 0, sức nặng 0
+                } else if (w[i - 1] <= j) {
+                    //Thỏi vàng thứ i ,
+                    result[i][j] = Math.max(result[i - 1][j], result[i - 1][j - w[i    - 1] + w[i]);
+                } else {
+                    result[i][j] = result[i - 1][j];
+                }
+            }
+        }
+        /*for (int i = 0; i < w.length; i++) {
           if (result + w[i] <= W) {
             result += w[i];
           }
-        }
-        return result;
+        }*/
+        return result[length][W];
     }
 
     public static void main(String[] args) {
